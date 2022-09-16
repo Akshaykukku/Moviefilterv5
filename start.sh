5cc65df4230eabdb01ed8d935631dace4fbd7087
CINEMA PRANTHAN <89033353+NEMEZIZ2311@users.noreply.github.com>
2022-09-16 23:35:49 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Akshaykukku/Moviefilterv5.git /Moviefilterv5.git
+  git clone https://github.com/Akshaykukku/Moviefilterv5.git /Moviefilterv5
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Moviefilterv5.git
+  git clone $UPSTREAM_REPO /Moviefilterv5
 fi
-cd /Moviefilterv5.git
+cd /Moviefilterv5
 pip3 install -U -r requirements.txt
 echo "STARTING MOVIEFILTER...."
 python3 bot.py
